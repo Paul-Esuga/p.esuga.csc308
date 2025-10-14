44168a62debbbb57383f68c4fdd8e3d57a4f30fb
Paul-Esuga <majiyeboesuga8@gmail.com>
2025-10-14 18:56:45 +0100

    Create grade function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn grade(score: i32) -> String {
+    let mut result= String::from("");
+    if score >= 90 {
+        result = String::from("Excellent!");
+    } else if score >= 70 {
+        result = String::from("Good job!");
+    } else if score < 40 {
+        result = String::from("Needs Remedial Practice.");
+    } else {
+        result = String::from("Needs improvement");
+    }
+    return result;
+}
+
 fn main() {
     println!("Hello, world!");
     let x = 5;
@@ -6,25 +20,24 @@ fn main() {
     y = 15;
     println!("Updated y : {}", y);
 
-    let age: i32 = 25;
-    let price: f64 = 19.99;
-    let is_member: bool = true;
-    let letter: char = 'R';
-    let name: &str = "Rustacean";
+    // let age: i32 = 25;
+    // let price: f64 = 19.99;
+    // let is_member: bool = true;
+    // let letter: char = 'R';
+    // let name: &str = "Rustacean";
 
-    let person: (i32, f64, char) = (30, 5.9, 'M');
+    // let person: (i32, f64, char) = (30, 5.9, 'M');
     let _scores: [i32; 4] = [10, 20, 30, 40];
+
+    let mut name = String::from("");
+    println!("Hello, {}", name);
+    name.push_str("acean");
+    println!("Updated: {}", name);
 
 
     let score = 85;
-    if score >= 90 {
-        println!("Excellent!");
-    } else if score >= 70 {
-        println!("Good job!");
-    } else if score < 40 {
-        println!("Needs Remedial Practice.");
-    } else {
-        println!("Needs improvement");
-    }
+    let result = grade(score);
+    println!("Score: {}, Result: {}", score, result);
+
 
 }
