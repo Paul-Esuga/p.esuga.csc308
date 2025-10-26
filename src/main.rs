ed93048e7311f8be515b4a19ee70fec41200b4d3
Paul-Esuga <majiyeboesuga8@gmail.com>
2025-10-26 22:09:05 +0100

    implemented mutability and borrowing(week 3)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod extra;
-mod week1;
+// mod week1;
+mod week2;
 
 fn main() {
     extra::hello();
     // testing::test();
-    week1::tasks();
+    // week1::tasks();
+    week2::tasks();
 
 }
